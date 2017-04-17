05087962ce8ad4fc84a27a47240afad91bcef42d
Clare Harshey <clarshey@gmail.com>
2017-04-17 00:19:20 -0400

    Yiddish grammar all-in-one

--- Yiddish.py
+++ Yiddish.py
@@ -8,9 +8,9 @@ from nltk.compat import unicode_repr
 
 from nltk.parse.api import ParserI
 
-imperative_grammar = nltk.CFG.fromstring("""
+grammar = nltk.CFG.fromstring("""
 
-  S ->  VP 
+  S ->  S_imperative | S_indicative | S_subjunctive 
   VP -> V NP | V NP PP | V PP | V AVP | Aux Con_Neg VP | Aux_Con VP | V
   PP -> P NP
   V -> "Turn" | "Stop" | "Change" | "Go" | "stop" | "change" | "run"
@@ -25,8 +25,8 @@ imperative_grammar = nltk.CFG.fromstring("""
   Aux_Con -> "Don't" 
   """)
 
-sentence = "Do not run ".split()
-rd_parser = nltk.RecursiveDescentParser(imperative_grammar)
+sentence = "".split()
+rd_parser = nltk.RecursiveDescentParser(grammar)
 for tree in rd_parser.parse(sentence):
     print(tree)
     print("---" * 30)

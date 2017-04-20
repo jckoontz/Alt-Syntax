98f98c9698fba6f04a8d7a602e08b6b189f65545
Clare Harshey <clarshey@gmail.com>
2017-04-19 22:43:12 -0400

    added Parsing1.py

--- Parsing1.py
+++ Parsing1.py
@@ -16,7 +16,7 @@ import sys, nltk
 
 
 # define the name of your grammar here
-grammarFile = "featureGrammarSP.fcfg"
+grammarFile = "featureGrammar.fcfg"
 
 # loading the grammar
 fcfg = nltk.data.load(grammarFile)
